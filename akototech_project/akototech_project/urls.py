71e2cae20c9cac561f980fc854d653e0e251983c
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-08-05 23:14:47 +0100

    files rearranged, urls.py set up in homepage app

--- akototech_project/akototech_project/urls.py
+++ akototech_project/akototech_project/urls.py
@@ -19,12 +19,6 @@ from akototech_home import views
 
 
 urlpatterns = [
-    path("", views.landing_page, name="landing_page"),
-    path("home/", views.homepage, name="homepage"),
-    path("about/", views.about_page, name="about_page"),
-    path("tuition/", views.akototech_tuition, name="akototech_tuition"),
-    path("projects/", views.akototech_projects, name="akototech_projects"),
-    path("ref/", views.references, name="references"),
-    path("contact/", views.contact_me, name="contact_me"),
+    path("", include("akototech_home.urls")),
     path("admin/", admin.site.urls),
 ]
