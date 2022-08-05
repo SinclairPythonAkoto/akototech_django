4730c5739d558c102260cabc95b171268f7b9065
SinclairPythonAkoto <sinclair.python@gmail.com>
2022-08-05 23:09:44 +0100

    HttpResponse obects created for main pages of homepage

--- akototech_project/akototech_home/views.py
+++ akototech_project/akototech_home/views.py
@@ -2,5 +2,45 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+# landing page (first page)
 def landing_page(request):
     return HttpResponse("Hello World")
+
+
+# main home page
+def homepage(request):
+    return HttpResponse("Welcome to the Homepage.")
+
+
+# about page
+def about_page(request):
+    return HttpResponse("About page - brief description of my passion and my goals")
+
+
+# AT tuition page
+def akototech_tuition(request):
+    return HttpResponse(
+        "Akoto Tech Tuition - classes available for anyone who wants to learn!"
+    )
+
+
+# projects page (EASYChef, Miri model, digital cv, dj fire)
+def akototech_projects(request):
+    return HttpResponse(
+        "Browse through some projects - EASY Chef, Miri the model, DJ Fyre"
+    )
+
+
+# reference page (modenised cv with download)
+def references(request):
+    return HttpResponse(
+        "References Page - insights from previous work and CV available to download!"
+    )
+
+
+# contact me page
+def contact_me(request):
+    return HttpResponse(
+        "Contact Me Page - users can send me an email to get in contact."
+    )
--- akototech_project/akototech_project/urls.py
+++ akototech_project/akototech_project/urls.py
@@ -20,5 +20,11 @@ from akototech_home import views
 
 urlpatterns = [
     path("", views.landing_page, name="landing_page"),
+    path("home/", views.homepage, name="homepage"),
+    path("about/", views.about_page, name="about_page"),
+    path("tuition/", views.akototech_tuition, name="akototech_tuition"),
+    path("projects/", views.akototech_projects, name="akototech_projects"),
+    path("ref/", views.references, name="references"),
+    path("contact/", views.contact_me, name="contact_me"),
     path("admin/", admin.site.urls),
 ]
